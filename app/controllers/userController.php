ec84c7ae1cbdaf4052c069abe25ec59d0c3583e2
AlbertoSanchez13 <aasanchez@est.istg.com.ec>
2024-07-16 17:27:40 -0500

    implementacion de codigo en archivo usuarioAjax -> que es el que recive las peticiones ajax del modulo usuarios, add-> creacion de funcion registrarUsuarioControlador

---
+++ app/controllers/userController.php
@@ -0,0 +1,15 @@
+<?php
+
+    namespace app\controllers;
+    use app\models\mainModel;
+
+
+    /* aqui userController hereda de mainModel */
+    class userController extends mainModel
+    {   
+        /* controlador para registros de usuarios, usado en el archivo usuarioAjax */
+        public function registrarUsuarioControlador(){
+
+        }
+
+    }
